0da7a52135932613ce509fb5282a93c180c370d1
Tafuso <gustavovasquez2002@gmail.com>
2022-01-13 18:32:17 -0300

    config port

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3333
+const port = process.env.PORT || 3333
 const mongoose = require('mongoose')
 require('dotenv').config()
 const router = require('./routes/router')
